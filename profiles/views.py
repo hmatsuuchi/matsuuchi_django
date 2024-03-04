591c37a82e0acdbbe17086fcddabd918217679e2
Hiroki Matsuuchi <hmatsuuchi@gmail.com>
2024-03-04 23:58:20 +0900

    added projects page with placeholder data

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from projects.models import Projects
 
 
 class IndexView(View):
@@ -8,7 +9,14 @@ class IndexView(View):
 
 class HirokiView(View):
     def get(self, request):
-        return render(request, 'profiles/hiroki.html')
+
+        projects = Projects.objects.all().filter(user=2)
+
+        context = {
+            'projects': projects
+        }
+
+        return render(request, 'profiles/hiroki.html', context)
     
 class ObitanView(View):
     def get(self, request):
--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Projects(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField()
-    url = models.URLField()
+    project_name = models.CharField(max_length=100)
+    project_subtitle = models.CharField(max_length=100)
+    project_description = models.TextField()
+
+    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
+
+    creation_date = models.DateField(default='2021-01-01')
+    update_date = models.DateField(default='2021-01-01')
+
+    url_stub = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = "Project"
         verbose_name_plural = "Projects"
 
     def __str__(self):
-        return self.name
+        return f"{self.project_name} - {self.creation_date} ({self.user})"
